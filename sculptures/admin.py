6cdb17fd4b4107492ba31d1008e6eba3bb9ffbf4
Ken Sheridan <kensheridan86@gmail.com>
2022-12-08 13:57:48 +0000

    scuplture model added to admin

--- sculptures/admin.py
+++ sculptures/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Sculpture
 
-# Register your models here.
+
+@admin.register(Sculpture)
+class SculptureAdmin(admin.ModelAdmin):
+    list_filter = ('created_on', 'title')
+    list_display = ('title', 'created_on')
+    search_fields = ('title',)

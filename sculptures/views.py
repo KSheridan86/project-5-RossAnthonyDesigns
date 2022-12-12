ba3ba79b07bec9b0c2f739921caa140e3afbf891
Ken Sheridan <kensheridan86@gmail.com>
2022-12-12 18:05:20 +0000

    product sorting added to the shop page

--- sculptures/views.py
+++ sculptures/views.py
@@ -5,9 +5,30 @@ from .models import Sculpture
 def shop(request):
     page = 'shop'
     sculptures = Sculpture.objects.all()
+
+    sort = None
+    direction = None
+
+    if request.GET:
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'title':
+                sortkey = 'lower_title'
+                sculptures = sculptures.annotate(lower_title=Lower('title'))
+
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            sculptures = sculptures.order_by(sortkey)
+
+    current_sorting = f'{sort}_{direction}'
+
     context = {
         'sculptures': sculptures,
-        'page': page
+        'page': page,
+        'current_sorting': current_sorting
     }
     return render(request, 'sculptures/shop.html', context)
 

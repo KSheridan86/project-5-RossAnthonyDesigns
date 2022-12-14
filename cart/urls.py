7dd04514db0ea38d959c0dd7e16a506c0b06d228
Ken Sheridan <kensheridan86@gmail.com>
2022-12-14 11:07:07 +0000

    add/subtract/remove item from cart functionality added

--- cart/urls.py
+++ cart/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.view_cart, name="view_cart"),
     path('add/<item_id>/', views.add_to_cart, name="add_to_cart"),
+    path('subtract/<item_id>/', views.subtract_from_cart, name="subtract_from_cart"),
+    path('delete/<item_id>/', views.delete_item_from_cart, name="delete_item_from_cart"),
     path('delete_cart/', views.delete_cart, name="delete_cart"),
 ]
--- cart/views.py
+++ cart/views.py
@@ -23,6 +23,36 @@ def add_to_cart(request, item_id):
     return redirect(redirect_url)
 
 
+def subtract_from_cart(request, item_id):
+    quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
+    cart = request.session.get('cart', {})
+
+    if quantity == 1:
+        del cart[item_id]
+    elif quantity > 1:
+        if item_id in list(cart.keys()):
+            cart[item_id] = quantity-1
+    else:
+        quantity = quantity
+
+    print(cart)
+    request.session['cart'] = cart
+    print(quantity)
+    return redirect(redirect_url)
+
+
+def delete_item_from_cart(request, item_id):
+    redirect_url = request.POST.get('redirect_url')
+    cart = request.session.get('cart', {})
+
+    if item_id in list(cart.keys()):
+        del cart[item_id]
+
+    request.session['cart'] = cart
+    return redirect(redirect_url)
+
+
 def delete_cart(request):
     request.session['cart'] = {}
     return redirect('view_cart')

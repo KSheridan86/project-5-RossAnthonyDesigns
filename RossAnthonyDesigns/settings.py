280843838e4b366f163b5ae9cffe8b942eb63a25
Ken Sheridan <kensheridan86@gmail.com>
2022-12-14 17:52:56 +0000

    updated styles for base/shop/single-item templates

--- RossAnthonyDesigns/settings.py
+++ RossAnthonyDesigns/settings.py
@@ -159,8 +159,8 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
-FREE_DELIVERY_THRESHOLD = 50
-STANDARD_DELIVERY_PERCENTAGE = 10
+FREE_DELIVERY_THRESHOLD = 1000
+STANDARD_DELIVERY_PERCENTAGE = 5
 
 
 # Default primary key field type

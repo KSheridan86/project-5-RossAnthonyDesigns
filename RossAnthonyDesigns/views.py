b8540f30933d87ddd56de16f2d90f8b450d6ac71
Ken Sheridan <kensheridan86@gmail.com>
2023-01-11 15:50:49 +0000

    updated 500 error template

--- RossAnthonyDesigns/views.py
+++ RossAnthonyDesigns/views.py
@@ -8,4 +8,4 @@ def handler404(request, exception):
 
 def handler500(request):
     """ Error Handler 500 - Server Error """
-    return render(request, "errors/404.html", status=500)
+    return render(request, "errors/500.html", status=500)

55a7c9c857a4c7c637ca8149a834eb42fb33520a
Ken Sheridan <kensheridan86@gmail.com>
2023-01-15 19:57:50 +0000

    order confirmation email added

--- users/signals.py
+++ users/signals.py
@@ -6,16 +6,41 @@ from django.contrib.auth.models import User
 from .models import Message, Review
 
 
+@receiver(post_save, sender=User)
+def new_user(sender, instance, created, **kwargs):
+    if created:
+        email = User.objects.all().order_by(
+            '-date_joined').values_list('email', flat=True)[0:1]
+        name = User.objects.all().order_by(
+            '-date_joined').values_list('username', flat=True)[0:1]
+        line = '\n'
+        send_mail(
+            'Welcome',
+            f"\
+            Hi {name[0]},{line}\
+            {line}\
+            We're happy you chose to register for an account.{line}\
+            You can now leave reviews and avail of any special friends{line}\
+            of Forged Nature discounts that pop up occasionally.{line}\
+            {line}\
+            Kind Regards,{line}\
+            The Forged Nature Team.",
+            'bobthewind420@gmail.com',
+            [email[0]],
+            fail_silently=False,)
+
+
 @receiver(post_save, sender=Message)
 def new_private_message(sender, instance, created, **kwargs):
     email = Message.objects.values_list('email', flat=True)[0:1]
     sender = Message.objects.values_list('name', flat=True)[0:1]
     if created:
-        send_mail('New Message',
-                  f'A new private message has been received from {sender[0]}.',
-                  email[0],
-                  ['kensheridan86@gmail.com'],
-                  fail_silently=False,)
+        send_mail(
+            'New Message',
+            f'A new private message has been received from {sender[0]}.',
+            email[0],
+            ['kensheridan86@gmail.com'],
+            fail_silently=False,)
 
 
 @receiver(post_save, sender=Review)
@@ -23,8 +48,9 @@ def new_review(sender, instance, created, **kwargs):
     name = Review.objects.values_list('name', flat=True)[0:1]
     email = Review.objects.values_list('email', flat=True)[0:1]
     if created:
-        send_mail('New Review',
-                  f'{name[0]} has submitted a review for consideration',
-                  email[0],
-                  ['kensheridan86@gmail.com'],
-                  fail_silently=False,)
+        send_mail(
+            'New Review',
+            f'{name[0]} has submitted a review for consideration',
+            email[0],
+            ['kensheridan86@gmail.com'],
+            fail_silently=False,)
